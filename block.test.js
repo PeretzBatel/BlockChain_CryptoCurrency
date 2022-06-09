5b2e12e0d3ad5b16a678182c0dc255faabb96d92
בת אל פרץ <Peretzbatel@nitc.co.il>
2022-06-09 12:35:07 +0300

    test

--- block.test.js
+++ block.test.js
@@ -1,4 +1,5 @@
 const Block = require('./block');
+const {DIFFICULTY} = require('./config');
 
 describe('Block', () => {
     let data, lastBlock, block; 
@@ -15,5 +16,16 @@ it('sets the `data` to match the input', ()=>{
 // another test, similar check for the arrtibutes, but this time checks the last hash
 it('sets the `lastHash` to match the hash of the last block', ()=>{
 expect(block.lastHash).toEqual(lastBlock.hash);
+});
+
+it('generates a hash that matches the difficulty', ()=> {
+expect(block.hash.substring(0, block.difficulty)).toEqual('0'.repeat(block.difficulty));
+});
+
+it('lowers the difficulty for slowly mined blocks', () => {
+    expect(Block.adjustDifficulty(block, block.timestamp+ 3600))
+    .toEqual(block.difficulty-1);
+
 })
+
 });
