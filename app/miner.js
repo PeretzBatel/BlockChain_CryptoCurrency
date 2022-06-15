ecf82cfc677c200a9ebded10ba8fb401a762f580
בת אל פרץ <Peretzbatel@nitc.co.il>
2022-06-15 12:39:47 +0300

    Miner

---
+++ app/miner.js
@@ -0,0 +1,19 @@
+
+class Miner {
+constructor(blockchain, transactionPool, wallet, p2pServer) {
+    this.blockchain = blockchain;
+    this.transactionPool = transactionPool;
+    this.wallet = wallet;
+    this.p2pServer = p2pServer;
+}
+    mine() {
+        const validTransactions = this.transactionPool.validTransactions();
+        // include reward for the miner
+        // create a block consisting of the valid transactions 
+        // sync the chains in the peer-to-peer server
+        // broadcast to every miner to clear their transaction pool
+    }
+
+}
+
+module.exports = Miner;
